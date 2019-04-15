404d391a8ad05ef25cb5e1ab0d8e823dc4af5144
Kshitiz Saini <40308662+kshitizsaini113@users.noreply.github.com>
2019-04-15 23:15:31 +0530

    Structure created

--- sdb.cpp
+++ sdb.cpp
@@ -8,7 +8,22 @@ using namespace std;
  int main()
 {
 
+struct student e;
+    char xfirst_name[50], xlast_name[50];
+    long int recsize;
 
+    fp=fopen("users.txt","rb+");
+
+    if (fp == NULL)
+    {
+        fp = fopen("users.txt","wb+");
+
+        if (fp==NULL)
+        {
+            puts("Cannot open file");
+            return 0;
+        }
+    }
 
 return 0;
 }
